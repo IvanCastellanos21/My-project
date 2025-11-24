f5239838a6d399f4e108775ca9da7cf9d0b21166
IvanCastellanos21 <ivancastellanos84@aragon.unam.mx>
2025-11-24 00:36:00 -0600

    Build completo

--- Assets/Scripts/PlayerColision.cs
+++ Assets/Scripts/PlayerColision.cs
@@ -5,28 +5,183 @@ using UnityEngine.UI;
 
 public class PlayerColision : MonoBehaviour
 {
+    //Audios
     public AudioSource reproductor;
-    public AudioClip selectNoise;
-    public Image bowlFrutas;
+    public AudioClip crowd_cheer;
+    public AudioClip fabrica;
+    public AudioClip gasolinera;
+
+    //Videos
+    public GameObject videoRandom;
+
+    //Imagenes
+    public Image frutas;
+    public Image panaderia;
+    public Image ComidaRapida;
+    public Image CoffeeShop;
+    public Image bar;
+    public Image farmacia;
+    public Image PolloFrito;
+    public Image restaurante;
+    public Image TiendaRegalos;
+    public Image pizzeria;
+
 
     void Start()
     {
-        bowlFrutas.gameObject.SetActive(false);
+        //Imagenes
+        frutas.gameObject.SetActive(false);
+        panaderia.gameObject.SetActive(false);
+        ComidaRapida.gameObject.SetActive(false);
+        CoffeeShop.gameObject.SetActive(false);
+        bar.gameObject.SetActive(false);
+        farmacia.gameObject.SetActive(false);
+        PolloFrito.gameObject.SetActive(false);
+        restaurante.gameObject.SetActive(false);
+        TiendaRegalos.gameObject.SetActive(false);
+        pizzeria.gameObject.SetActive(false);
+        
+        //Videos
+        videoRandom.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider obj)
     {
+        //Imagenes
         if (obj.tag == "fruteria")
         {
-            bowlFrutas.gameObject.SetActive(true);
+            frutas.gameObject.SetActive(true);
+        }
+
+        if (obj.tag == "panaderia")
+        {
+            panaderia.gameObject.SetActive(true);
+        }
+
+        if (obj.tag == "farmacia")
+        {
+            farmacia.gameObject.SetActive(true);
+        }
+
+        if (obj.tag == "ComidaRapida")
+        {
+            ComidaRapida.gameObject.SetActive(true);
+        }
+
+        if (obj.tag == "PolloFrito")
+        {
+            PolloFrito.gameObject.SetActive(true);
+        }
+
+        if (obj.tag == "restaurante")
+        {
+            restaurante.gameObject.SetActive(true);
+        }
+
+        if (obj.tag == "TiendaRegalos")
+        {
+            TiendaRegalos.gameObject.SetActive(true);
+        }
+
+         if (obj.tag == "CoffeeShop")
+        {
+            CoffeeShop.gameObject.SetActive(true);
+        }
+
+        if (obj.tag == "bar")
+        {
+            bar.gameObject.SetActive(true);
+        }
+
+        if (obj.tag == "pizzeria")
+        {
+            pizzeria.gameObject.SetActive(true);
+        }
+
+        //Videos
+        if (obj.tag == "cine")
+        {
+            videoRandom.SetActive(true);
+        }
+
+        //Ruidos one shot
+        if (obj.tag == "estadio")
+        {
+            reproductor.PlayOneShot(crowd_cheer);
+        }
+
+        if (obj.tag == "gasolinera")
+        {
+            reproductor.PlayOneShot(gasolinera);
+        }
+
+        if (obj.tag == "fabrica")
+        {
+            reproductor.PlayOneShot(fabrica);
         }
     }
     
     private void OnTriggerExit(Collider obj)
     {
-        if(obj.tag == "fruteria")
+        // Audio
+        reproductor.Stop();
+
+        //Imagenes
+        if (obj.tag == "fruteria")
         {
-            bowlFrutas.gameObject.SetActive(false);
+            frutas.gameObject.SetActive(false);
+        }
+
+        if (obj.tag == "panaderia")
+        {
+            panaderia.gameObject.SetActive(false);
+        }
+
+         if (obj.tag == "CoffeeShop")
+        {
+            CoffeeShop.gameObject.SetActive(false);
+        }
+
+        if (obj.tag == "farmacia")
+        {
+            farmacia.gameObject.SetActive(false);
+        }
+
+        if (obj.tag == "ComidaRapida")
+        {
+            ComidaRapida.gameObject.SetActive(false);
+        }
+
+        if (obj.tag == "PolloFrito")
+        {
+            PolloFrito.gameObject.SetActive(false);
+        }
+
+        if (obj.tag == "restaurante")
+        {
+            restaurante.gameObject.SetActive(false);
+        }
+
+        if (obj.tag == "TiendaRegalos")
+        {
+            TiendaRegalos.gameObject.SetActive(false);
+        }
+
+        if (obj.tag == "bar")
+        {
+            bar.gameObject.SetActive(false);
+        }
+
+        if (obj.tag == "pizzeria")
+        {
+            pizzeria.gameObject.SetActive(false);
+        }
+
+
+        // Videos
+        if (obj.tag == "cine")
+        {
+            videoRandom.SetActive(false);
         }
     }
 }
